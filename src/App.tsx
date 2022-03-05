7d4634ef20a66bc35424e55d565eb0ae83fef13e
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-03-05 11:39:57 +0700

    clean up & format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,9 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p className='my-4 px-6 py-1 bg-blue-200 text-gray-700 rounded'>
-          Halo!
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="w-screen bg-gray-800 min-h-screen p-2">
+      <div className="w-20">
+        <p className="w-full text-center bg-blue-200 text-gray-700 rounded">Halo!</p>
+      </div>
     </div>
   )
 }
