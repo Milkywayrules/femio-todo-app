5a60c6c1451da9e40c5454a008b79b411ffc3abf
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-03-05 12:17:23 +0700

    style: setup a new font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="w-screen bg-gray-800 min-h-screen p-2">
+    <div className="w-screen bg-gray-800 min-h-screen p-2 font-main">
       <div className="w-20">
         <p className="w-full text-center bg-blue-200 text-gray-700 rounded">Halo!</p>
       </div>
