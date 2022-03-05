e227c3ad4499fd000425a679cfd2512832a94093
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-03-05 13:58:33 +0700

    cnfg: add styles to tw conf
    follow the requirements style guidelines

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,44 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      main: ['Josefin Sans', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },
+    colors: {
+      gray: {
+        l: {
+          100: 'hsl(0, 0%, 98%)',
+          200: 'hsl(236, 33%, 92%)',
+          300: 'hsl(233, 11%, 84%)',
+          400: 'hsl(236, 9%, 61%)',
+          500: 'hsl(235, 19%, 35%)',
+        },
+        d: {
+          100: 'hsl(234, 39%, 85%)',
+          200: 'hsl(236, 33%, 92%)',
+          300: 'hsl(234, 11%, 52%)',
+          400: 'hsl(233, 14%, 35%)',
+          500: 'hsl(237, 14%, 26%)',
+        },
+      },
+      blue: {
+        'bright': 'hsl(220, 98%, 61%)',
+        'dark': 'hsl(235, 21%, 11%)',
+        'dark-desaturated': 'hsl(235, 24%, 19%)',
+      },
+      check: {
+        'linear-gradient': 'hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+      },
+    },
     extend: {
-      fontFamily: {
-        main: ['Josefin Sans', 'sans-serif'],
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
       },
     },
   },
