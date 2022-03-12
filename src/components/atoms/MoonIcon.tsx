499f2b2f589a34f7486b85b7d127dfe0939ac696
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-03-12 16:02:15 +0700

    add: SunIcon & MoonIcon atoms

---
+++ src/components/atoms/MoonIcon.tsx
@@ -0,0 +1,7 @@
+import moonIcon from '@/assets/icons/icon-moon.svg'
+
+const MoonIcon = () => (
+  <img src={moonIcon} alt="Sun Icon" className="h-5 w-5 desktop:h-8 desktop:w-8" />
+)
+
+export default MoonIcon
---
+++ src/components/atoms/SunIcon.tsx
@@ -0,0 +1,7 @@
+import sunIcon from '@/assets/icons/icon-sun.svg'
+
+const SunIcon = () => (
+  <img src={sunIcon} alt="Sun Icon" className="h-5 w-5 desktop:h-8 desktop:w-8" />
+)
+
+export default SunIcon
