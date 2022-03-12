ad666989a27a9c75eed574e1be9d04c1f08a2188
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-03-12 17:07:52 +0700

    refactor: restructure todo item UX

--- src/App.tsx
+++ src/App.tsx
@@ -92,31 +92,50 @@ const App = () => {
 
           <TodoContainer todosState={[todos, setTodos]} filterState={[filterOn, setFilterOn]}>
             {({ id, isComplete, todo }) => {
-              let className = ''
-              let classNameP = 'text-gray-l-500 dark:text-gray-d-100'
-
-              if (isComplete) {
-                className = 'bg-gradient-to-br from-check-1 to-check-2'
-                classNameP = 'line-through text-gray-l-300 dark:text-gray-d-300'
-              }
-
               return (
                 <Fragment key={id}>
                   <div className="flex w-full items-center justify-between gap-3 overflow-hidden bg-white py-3.5 px-4 text-sm dark:bg-gray-d-400">
                     <div
-                      className="flex w-full items-center gap-3"
+                      className={`flex w-full items-center gap-3 hover:cursor-pointer focus-visible:bg-blue-bright ${
+                        isComplete
+                          ? 'text-gray-l-300 dark:text-gray-d-300'
+                          : 'text-gray-l-500 dark:text-gray-d-100'
+                      }`}
                       onClick={() => handleTodoComplete(id)}
                     >
-                      <div
-                        className={`flex h-5 w-5 shrink-0 items-center justify-center rounded-full border border-gray-l-300 dark:border-gray-d-300 ${className}`}
+                      {/* bullet outer */}
+                      <button
+                        className={`group flex h-5 w-5 shrink-0 rounded-full bg-gray-l-300 p-[1px] outline-none hover:bg-gradient-to-br hover:from-check-1 hover:to-check-2 focus-visible:bg-gradient-to-br focus-visible:from-check-1 focus-visible:to-check-2 dark:bg-gray-d-300 ${
+                          isComplete ? 'bg-gradient-to-br from-check-1 to-check-2' : ''
+                        }`}
                       >
-                        {isComplete && <img src={checkIcon} alt="check icon" className="h-2 w-2" />}
-                      </div>
-                      <p className={`h-full w-full text-lg ${classNameP}`}>{todo}</p>
+                        {/* bullet inner */}
+                        <div
+                          className={`flex h-full w-full shrink-0 items-center justify-center rounded-full ${
+                            isComplete
+                              ? 'group-focus-visible:bg-transparent'
+                              : 'bg-white dark:bg-gray-d-400'
+                          }`}
+                        >
+                          {isComplete && (
+                            <img src={checkIcon} alt="check icon" className="h-2 w-2" />
+                          )}
+                        </div>
+                      </button>
+                      <p
+                        className={`h-full w-full text-left text-lg ${
+                          isComplete ? 'line-through' : ''
+                        }`}
+                      >
+                        {todo}
+                      </p>
                     </div>
 
-                    <button className="shrink-0" onClick={() => handleTodoDelete(id)}>
-                      <Icons.cross className="h-5 w-5 fill-gray-d-300/50" />
+                    <button
+                      className="shrink-0 rounded outline-none hover:bg-gray-l-200 focus-visible:bg-gray-l-200 active:bg-gray-l-200/50 dark:hover:bg-gray-d-500/50 dark:focus-visible:bg-gray-d-500/50 dark:active:bg-gray-d-500"
+                      onClick={() => handleTodoDelete(id)}
+                    >
+                      <Icons.cross className="h-5 w-5 fill-gray-d-300/50 dark:fill-gray-d-300" />
                     </button>
                   </div>
                 </Fragment>
